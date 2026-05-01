061402be8d42cc9fec89eb99bdabf7333db1db97
mansi <mansiakkr@gmail.com>
2026-05-02 00:06:50 +0530

    feat: add RestClient for third party API integration with logging configuration and error handling

---
+++ src/main/java/com/springboot/prodreadyfeatures/clients/EmployeeClient.java
@@ -0,0 +1,15 @@
+package com.springboot.prodreadyfeatures.clients;
+
+import com.springboot.prodreadyfeatures.dto.EmployeeDTO;
+
+import java.util.List;
+
+public interface EmployeeClient {
+
+    List<EmployeeDTO> getAllEmployees();
+
+    EmployeeDTO getEmployeeById(Long employeeId);
+
+    EmployeeDTO createNewEmployee(EmployeeDTO employeeDTO);
+
+}
